77243295826853367f0a23a1f295bb35774f0b61
psoulier <paul.soulier@gmail.com>
2015-09-09 12:19:26 -1000

    New sample code

--- Test.java
+++ Test.java
@@ -3,20 +3,33 @@
  */
 public class Test {
 
+  /**
+   * Implements the Ackermann function.
+   */
+  public static long ackermann(long m, long n) {
+    Jog.debug("ackermann with m, n=" + m + ", " + n);
+    if (m == 0) {
+      Jog.info("Returning from recursion (m, n)= " + m + ", " + n);
+      return n+1;
+    }
+    else if (m > 0 && n == 0) {
+      return ackermann(m-1, 1);
+    }
+    else {
+      return ackermann(m-1, ackermann(m, n-1));
+    }
+  }
+
   public static void main(String[] args) {
     // Allocate a memory logger and tell Jog to use it for logging.
     Jog.setLogger(new MemoryLogger());
 
-    // Enable DEBUG log entries.
+    // Enable DEBUG and INFO log entries.
     Jog.enableLevel(Level.DEBUG);
+    Jog.enableLevel(Level.INFO);
 
-    for (int i = 0; i < 29; i++) {
-      Jog.debug("Log entry " + i);
-    }
 
-    // You'll notice this doesn't get printed out since Level.INFO isn't enabled.
-    Jog.info("An INFO log entry");
-
+    System.out.println(ackermann(2, 2));
     Jog.printLog();
   }
 
